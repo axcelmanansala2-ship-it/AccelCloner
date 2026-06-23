689eefeec3e05e97cdb81419ce9a27e89f9653fc
axcelmanansala2-ship-it <axcelmanansala2@gmail.com>
2026-06-23 14:21:18 +0800

    fix: merge AppInfo+ClonedApp, add sourceApk/sizeBytes

--- app/src/main/java/com/accel/cloner/core/AppInfo.kt
+++ app/src/main/java/com/accel/cloner/core/AppInfo.kt
@@ -9,6 +9,17 @@ data class AppInfo(
     val versionName: String,
     val versionCode: Long,
     val isSystemApp: Boolean,
+    val sourceApk: String = "",
+    val sizeBytes: Long = 0L,
     val isCloned: Boolean = false,
     val cloneIndex: Int = 0
+)
+
+data class ClonedApp(
+    val packageName: String,
+    val appName: String,
+    val icon: Drawable?,
+    val cloneIndex: Int,
+    val virtualDataPath: String,
+    val sizeBytes: Long
 )
